6752a3f2bf498cefab52ff082861e8e39fa6fe6a
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-13 14:33:25 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -36,14 +36,10 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text="اضف البوت الى مجموعتڪ ✅",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
+            ), 
             InlineKeyboardButton(
                 text="༺༽الاوامر༼༻", callback_data="settings_back_helper"
-           )
-        ],
-        [
+           ), 
             InlineKeyboardButton(
                 text=" مطور البوت 👨‍💻", user_id=OWNER
             )
