616bee57579f0fcf2702da5f3fde84d8b4e9b5fa
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-13 07:55:10 +0300

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,9 +48,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("OneShotFlash")
+                await self.one.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(1)
@@ -80,9 +80,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
-                await self.one.join_chat("OneShotFlash")
+                await self.two.join_chat("ah07v")
+                await self.two.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(2)
@@ -112,9 +112,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
-                await self.one.join_chat("OneShotFlash")
+                await self.three.join_chat("ah07v")
+                await self.three.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(3)
@@ -144,9 +144,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
-                await self.one.join_chat("OneShotFlash")
+                await self.four.join_chat("ah07v")
+                await self.four.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(4)
@@ -176,9 +176,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
-                await self.one.join_chat("OneShotFlash")
+                await self.five.join_chat("ah07v")
+                await self.five.join_chat("ah07v")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(5)
