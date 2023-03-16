1f9d77740045d431f10f0e4c182d0a93ab19f4da
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-16 20:25:09 +0300

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -41,6 +41,12 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text="✪ اوامر المطور ✪",
                     callback_data="help_callback hb9",
+                    )
+        ],
+        [
+            InlineKeyboardButton(
+            text="󠁧󠁢󠁥🇪🇬 عربي",
+            callback_data=f"languages:en",
                 ),
             ],
             mark,
